784b0f950bb38b41e6ce414e43a78b4a8771fd39
ggzhangna <ggzhangna@163.com>
2020-06-26 13:04:34 +0800

    表

---
+++ com.zn.bio/src/main/java/com/zn/bio/demo/TimeClient.java
@@ -0,0 +1,64 @@
+package com.zn.bio.demo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ * TimeClient
+ *
+ * @author ggzhangna
+ * @date 20/6/25
+ */
+public class TimeClient {
+    public static void main(String[] args) {
+        int port = 8080;
+        if (args != null && args.length > 0){
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e){
+                System.out.println("use default port.");
+            }
+        }
+
+        Socket socket = null;
+        BufferedReader in = null;
+        PrintWriter out = null;
+        try {
+            socket = new Socket("127.0.0.1",port);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream(),true);
+            out.println("query time order");
+
+            System.out.println("send order 2 server succeed.");
+            String resp = in.readLine();
+            System.out.println("Now is : " + resp);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (out != null){
+                out.close();
+                out = null;
+            }
+
+            if (in != null){
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                in = null;
+            }
+            if (socket != null){
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                socket = null;
+            }
+        }
+    }
+}
